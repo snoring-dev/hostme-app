12db8d8337a133d87d6a305f93c7f2a4d46984ae
snoring-dev <jemmoudi.med@proton.me>
2023-06-20 13:49:57 +0200

    feat(renting): add third step of renting process

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -10,6 +10,7 @@ import { categories } from "@/app/utils/categories";
 import CategoryInput from "../inputs/CategoryInput";
 import { FieldValues, useForm } from "react-hook-form";
 import CountrySelect from "../inputs/CountrySelect";
+import Counter from "../inputs/Counter";
 
 function RentModal() {
   const rentModal = useRentModal();
@@ -38,6 +39,9 @@ function RentModal() {
 
   const category = watch("category");
   const location = watch("location");
+  const guestCount = watch("guestCount");
+  const roomCount = watch("roomCount");
+  const bathroomCount = watch("bathroomCount");
 
   const customSetValue = (id: string, value: any) => {
     setValue(id, value, {
@@ -112,6 +116,37 @@ function RentModal() {
           onChange={(val) => customSetValue("location", val)}
         />
         <Map center={location?.latlng} />
+      </div>
+    );
+  }
+
+  if (step === HOSTING_STEPS.INFO) {
+    modalBody = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Share some basics about your place"
+          subtitle="What amenities do you have?"
+        />
+        <Counter
+          title="Guests"
+          subtitle="How many guests are allowed?"
+          value={guestCount}
+          onChange={(value) => customSetValue("guestCount", value)}
+        />
+        <hr />
+        <Counter
+          title="Rooms"
+          subtitle="How many rooms do you have?"
+          value={roomCount}
+          onChange={(value) => customSetValue("roomCount", value)}
+        />
+        <hr />
+        <Counter
+          title="Bathrooms"
+          subtitle="How many bathrooms do you have?"
+          value={bathroomCount}
+          onChange={(value) => customSetValue("bathroomCount", value)}
+        />
       </div>
     );
   }
