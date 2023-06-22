80c94db83ce98f7ccebe496bfc67a6d348af7bb3
snoring-dev <jemmoudi.med@proton.me>
2023-06-22 22:42:42 +0200

    feat(api): add reservation endpoint & logic

---
+++ app/api/reservations/route.ts
@@ -0,0 +1,34 @@
+import { NextResponse } from "next/server";
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import prisma from "@/app/utils/prismadb";
+import isNil from "lodash/isNil";
+import { data } from "autoprefixer";
+
+export async function POST(request: Request) {
+  const currentUser = await getCurrentUser();
+  const body = await request.json();
+  const { listingId, startDate, endDate, totalPrice } = body;
+  const allValuesSet = Object.values(body).every((v) => !isNil(v));
+
+  if (!allValuesSet || !currentUser) {
+    return NextResponse.error();
+  }
+
+  const listingAndResa = await prisma.listing.update({
+    where: {
+      id: listingId,
+    },
+    data: {
+      reservations: {
+        create: {
+          userId: currentUser.id,
+          startDate,
+          endDate,
+          totalPrice,
+        },
+      },
+    },
+  });
+
+  return NextResponse.json(listingAndResa);
+}
